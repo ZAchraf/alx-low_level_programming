d98270f6527963fe0ed74dafa0cf141e7147036c
Ztote Achraf <achraf.ztote@gmail.com>
2023-10-05 20:55:02 -0700

    fifth task C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -  basic function
+ *
+ * Description : how to use puts
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
