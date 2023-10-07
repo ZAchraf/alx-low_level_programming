f8623233175cb7d672853b47b594e9e76349e963
Ztote Achraf <achraf.ztote@gmail.com>
2023-10-06 19:09:02 -0700

    my fifth task, i hope it will work from the first try

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  main - the main function
+ *Description : the for loop
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int  dig;
+
+	for (dig = 0; ch < 10; dig++)
+	{
+		printf("%d", dig);
+	}
+	putchar('\n');
+	return (0);
+}
