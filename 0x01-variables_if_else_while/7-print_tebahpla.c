66450aa59eb17fcdb52bef83eb82aabae1e6c46b
Ztote Achraf <achraf.ztote@gmail.com>
2023-10-06 19:29:06 -0700

    6th worked from the first try, hopefully the 7th will do as well

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+i#include <stdio.h>
+
+/**
+ *  main - the main function
+ *Description : the for loop
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char  ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
