60a6a5786a75085e774ee871f27591725c73a303
Ztote Achraf <achraf.ztote@gmail.com>
2023-10-06 19:18:31 -0700

    sixth task, wish me luck

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  main - the main function
+ *Description : the for loop
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int  dig;
+
+	for (dig = '0'; dig <= '9'; dig++)
+	{
+		putchar(dig);
+	}
+	putchar('\n');
+	return (0);
+}
