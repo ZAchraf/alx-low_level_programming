5b9b51db543d1a584c7cd814b1a5260c36843781
Ztote Achraf <achraf.ztote@gmail.com>
2023-10-06 19:51:12 -0700

    8th,wish me luck

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *  main - the main function
+ *Description : the for loop
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char  hexa;
+
+	for (hexa = 0; hexa <= 16; hexa++)
+	{
+		if (i < 10)
+		{
+			putchar('0' + hexa);
+		} else
+		{
+			putchar('a' + (hexa - 10);
+	}
+	putchar('\n');
+	return (0);
+}
