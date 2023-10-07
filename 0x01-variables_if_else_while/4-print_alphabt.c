9ad61a1c7168318cea8b1ae8c37cba581afe19d0
Ztote Achraf <achraf.ztote@gmail.com>
2023-10-06 18:32:13 -0700

    probably there is an error but i will take care of it, task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,11 +11,11 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if (ch == 'q' || ch == 'e')
+		if (ch != 'q' || ch != 'e')
 		{
-			putchar('');
+			putchar(ch);
 		}
-		putchar(ch);
+		putchar('\n');
 	}
 	return (0);
 }
