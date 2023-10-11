01cc2e5132948c9080e1fb1ece6b93ceacc06ea4
Ztote Achraf <achraf.ztote@gmail.com>
2023-10-11 13:28:38 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _abs - is a function that computes the absolute value
+ *
+ * @n: function input
+ *
+ * Return: the value of n
+*/
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+	return (n);
+	}
+	else if (n < 0)
+	{
+		n = n * (-1)
+		return (n);
+	}
+	else
+	{
+		return (0);
+	}
+}
