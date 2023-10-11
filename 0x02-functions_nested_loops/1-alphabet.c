f32a0ce3fe3e600388c809072b5c2835747939a4
Ztote Achraf <achraf.ztote@gmail.com>
2023-10-11 09:53:41 -0700

    second task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "mai.h"
+
+/**
+ * print_alphabet - is a function that uses another
+ *		function to print from a to z
+*/
+
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a' ; a < '{'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define main_h
 #include <stdio.h>
 
-int _putchar(char a);
+int _putchar(char c);
+void print_alphabet(void);
 
 #endif
