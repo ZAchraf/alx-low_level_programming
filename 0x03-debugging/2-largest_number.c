f27f1fe7a6a62112715d8f904c6c5ebce09704a0
Ztote Achraf <achraf.ztote@gmail.com>
2023-10-12 16:09:07 -0700

    task2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,12 +13,16 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if (a > b && a > c)
+	{
 		largest = a;
+	}
 	if (b > a && b > c)
+	{
 		largest = b;
+	}
 	else
+	{
 		largest = c;
-
+	}
 	return (largest);
-
 }
