31d006c5be3928692330c39901c922734ec17d71
Ztote Achraf <achraf.ztote@gmail.com>
2023-10-11 19:11:19 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - is a function that adds up the parameters
+ *
+ *@a: function imput
+ *
+ *@b: function imput
+ *
+ * Return: the final value
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
