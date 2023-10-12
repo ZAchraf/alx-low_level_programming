bdee9eb0668c873b2b93925a58c83d1ee0a488d5
Ztote Achraf <achraf.ztote@gmail.com>
2023-10-11 19:38:28 -0700

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_to_98 - it prints from n to 98
+ * @n : function input
+*/
+
+void print_to_98(int n)
+{
+	int j;
+
+	if (n <= 98)
+	{
+		for (j = n; j < 98; j++)
+		{
+			printf("%d, ", j);
+		}
+	}
+	else
+	{
+		for (j = n; j > 98; j--)
+			printf("%d, ", j);
+	}
+	printf("98\n");
