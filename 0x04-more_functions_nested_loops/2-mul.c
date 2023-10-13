d3185869fb8eaf9c438841fe958170dc76e3b2c5
Ztote Achraf <achraf.ztote@gmail.com>
2023-10-12 18:56:02 -0700

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - mutliplies to numbers
+ * @a: function imput
+ * @b: function imput
+ * Return: the multiplied valued of a and b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
