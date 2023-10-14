955a530b75c66292f928f5233c123438e236a39d
Ztote Achraf <achraf.ztote@gmail.com>
2023-10-13 19:07:30 -0700

    task7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - it draws a diagonal line
+ * @n: function imput
+*/
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= i; j++)
+			{
+				putchar(' ');
+			}
+			putchar('\');
+			putchar('\n');
+		}
+	}
+}
