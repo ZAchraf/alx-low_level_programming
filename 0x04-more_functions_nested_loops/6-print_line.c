63f74aa97cc397d809b2507f1f57cfc113d122a5
Ztote Achraf <achraf.ztote@gmail.com>
2023-10-13 18:26:07 -0700

    task6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,9 +9,16 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 1; i <= n; i++)
+	if (n <= 0)
 	{
-		_putchar(95);
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
 }
