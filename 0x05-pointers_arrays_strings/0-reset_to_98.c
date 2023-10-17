f7024e1cc090beb1e3c9c53dc6f3a72268d53038
Ztote Achraf <achraf.ztote@gmail.com>
2023-10-16 17:41:50 -0700

    task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - it updates the values it points to to 98
+ * @n: function input
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+pointers are a bit hard, but we are here to do hard things
