de49788ad60e9e538f51582dbf274ab8aa82e6d2
Ztote Achraf <achraf.ztote@gmail.com>
2023-10-23 19:06:53 -0700

    task4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strpbrk - it searches a string for any of a set of bytes
+ * @s: str
+ * @accept: str to match
+ * Return: pointer to the byte in s
+ * or Null if no such byte is found
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int ir, jr;
+
+	ir = 0;
+	while (s[r] != '\0')
+	{
+		jr = 0;
+		while (accept[jr] != '\0')
+		{
+			if (accept[jr] == s[ir])
+			{
+				p = &s[ir];
+				return (p);
+			}
+			jr++;
+		}
+		ir++;
+	}
+	return (0);
