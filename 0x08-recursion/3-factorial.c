0c4087a267e138f040853e718be506c26080184a
Ztote Achraf <achraf.ztote@gmail.com>
2023-10-25 18:20:48 -0700

    task3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,15 +8,13 @@
 
 int factorial(int n)
 {
-	int f;
-
 	if (n < 0)
 	{
 		return (-1);
 	}
 	else if (n == 0)
 	{
-		f = n * factorial(n - 1);
+		return (1);
 	}
-	return (f);
+	return (n * (factorial(n - 1)));
 }
