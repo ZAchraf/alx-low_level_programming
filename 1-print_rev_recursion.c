e2690753522e7de7251dfe88852430b4aeae7459
Ztote Achraf <achraf.ztote@gmail.com>
2023-10-25 17:37:49 -0700

    task1

---
+++ 1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string backward
+ * @s: is a pointer to a string
+ * Return: void
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+}
