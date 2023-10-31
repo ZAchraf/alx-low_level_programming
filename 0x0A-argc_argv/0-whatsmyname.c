a7d6a98efa045fbb82c3cab24fd16766cfae6f78
Ztote Achraf <achraf.ztote@gmail.com>
2023-10-30 19:14:10 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - is the main function
+ * @argc: how many argument
+ * @argv: contains all the strings
+ * Return: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf(argv[0]);
+
+	return (0);
+}
