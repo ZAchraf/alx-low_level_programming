da103385059de62e16f1d2231d3a263e0d772b26
Ztote Achraf <achraf.ztote@gmail.com>
2023-11-01 11:50:07 -0700

    task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,15 @@ int main(int argc, char *argv[])
 {
 	int m;
 
-	if (argc != 3)
+	if (argc == 3)
 	{
-		printf("Error\n");
-		return (1);
+		m = atoi(argv[1] * atoi(argv[2]);
+		printf("%d\n", m);
 	}
 	else
 	{
-	m = atoi(argv[1]) * atoi(argv[2]);
-	printf("%d\n", m);
+	printf("Error\n");
+	return (1);
 	}
 	return (0);
 }
