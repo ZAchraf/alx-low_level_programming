9aab2c2a3b5e9b0d1763079764a86e897b82b6f2
Ztote Achraf <achraf.ztote@gmail.com>
2023-11-01 17:56:03 -0700

    task4 after a lot of thinking

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+#include <ctype>
+
+/**
+ * main - adds positive numbers
+ * @argc: how many argument
+ * @argv: contains all the strings
+ * Return: 0 = success else fail
+*/
+
+int main(int argc, char *argv[])
+{
+	int S, i;
+	int c = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (isdigit(argv[i]))
+		{
+		;
+		}
+		else
+		{
+		c++;
+		}
+	}
+
+	if (argc < 2 && c == 0)
+	{
+		printf("0\n");
+	}
+	else if (argc > 1 && c == 0)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			S = S + atoi(argv[i]);
+		}
+		printf("%d", S);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
