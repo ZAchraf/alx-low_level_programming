ff0390e86f82ec884cee4c2135967dfde5b65ba4
Ztote Achraf <achraf.ztote@gmail.com>
2023-11-01 19:02:18 -0700

    task5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - prints minimum number of coins
+ * to make change for an amount of money
+ * @argc: number of arguments
+ * @argv: pointer to an array of arguments
+ * Return: 0-success, else fail
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int i;
+		int lc = 0;
+		int m = atoi(argv[1]);
+		int c[] = {25, 10, 5, 2, 1};
+
+		for (i = 0; i < 5; i++)
+		{
+			if (m >= c[i])
+			{
+				lc += m / c[i];
+				m = m % c[i];
+				if (n % c[i] == 0)
+				{
+					break;
+				}
+			}
+		}
+		printf("%d\n", lc);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
