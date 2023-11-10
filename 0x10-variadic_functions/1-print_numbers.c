250d4fffd50c874797a190843c4c9c6619eea085
Ztote Achraf <achraf.ztote@gmail.com>
2023-11-09 18:47:52 -0800

    task1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers -  prints numbers, followed by a new line
+ * @separator: a str
+ * @n: the variable or number of args
+ * Return: void
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	va_list nb;
+
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	va_start(nb, n);
+	for (i = 1; i <= n; i++)
+	{
+		printf("%d%s", va_arg(nb, int), i ? (separator ? separator : "") : "\n");
+	}
+	va_end(nb);
+}
