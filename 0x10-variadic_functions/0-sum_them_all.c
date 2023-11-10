c8f3b5c13c94425a8d1e1bc6f076ee9115cad23e
Ztote Achraf <achraf.ztote@gmail.com>
2023-11-09 17:27:10 -0800

    task0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - a function that adds up numbers
+ * @n: the variable or number of args
+ * Return: the sum or 0 if  n==0
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n == 0)
+		return (0);
+
+	va_list nb;
+	int i, sum;
+
+	va_start(nb, n);
+	for (i = 0; i < n; i++)
+	{
+		sum = sum + va_arg(nb, int);
+	}
+	va_end(nb);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+variadic functions, let's go
