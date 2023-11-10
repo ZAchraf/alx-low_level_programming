46ef876d6288e6ad5a7ddb80c154b0b0c867e56e
Ztote Achraf <achraf.ztote@gmail.com>
2023-11-09 18:14:26 -0800

    task0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,7 +8,7 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int i, sum = 0;
+	int i = n, sum = 0;
 	va_list nb;
 
 	if (n == 0)
@@ -16,7 +16,7 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 	}
 	va_start(nb, n);
-	for (i = 0; i < n; i++)
+	while (i--)
 	{
 		sum = sum + va_arg(nb, int);
 	}
