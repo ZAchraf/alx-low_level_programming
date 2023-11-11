264c4854980bb2afcd2a1e2597277b1ec365f0cd
Ztote Achraf <achraf.ztote@gmail.com>
2023-11-11 09:32:59 -0800

    task2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings -  prints the strings, followed by a new line at the end
+ * @separator: a str
+ * @n: the variable or number of args
+ * Return: void
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int i;
+
+	va_list v;
+
+	if (n == 0)
+	{
+		return;
+	}
+	va_start(v, n);
+	i = n;
+
+	while (i--)
+	{
+		char *sm = va_arg(v, char*);
+
+		printf("%s%s", sm ? sm : "nil",
+				i ? separator : "");
+	}
+	va_end (v);
+}
