04b3f8c1f7534cc4d8880afc0c7aca19594ff39e
Ztote Achraf <achraf.ztote@gmail.com>
2023-12-01 13:48:20 -0800

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -52,4 +52,4 @@ int create_file(const char *filename, char *text_content)
 	close(file_d);
 
 	return (b == length ? 1 : -1);
-        file_d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);}
+}
